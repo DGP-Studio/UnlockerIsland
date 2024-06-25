e86888f00b26ea8619488f2781b71969d4306f79
Lightczx <1686188646@qq.com>
2024-06-25 10:25:20 +0800

    1.0.9

--- src/UnlockerIsland/Snap.Hutao.UnlockerIsland/dllmain.h
+++ src/UnlockerIsland/Snap.Hutao.UnlockerIsland/dllmain.h
@@ -8,7 +8,6 @@
 constexpr auto ISLAND_FEATURE_HANDLE_DLL_PROCESS_DETACH = true;
 
 constexpr PCWSTR ISLAND_ENVIRONMENT_NAME = L"4F3E8543-40F7-4808-82DC-21E48A6037A7";
-constexpr PCWSTR ISLAND_FILE_NAME = L"Snap.Hutao.UnlockerIsland.dll";
 
 // This function is only meant to get an arbitrary function pointer from the DLL
 // So that we can use SetWindowHookEx to inject the DLL into the game
@@ -24,21 +23,9 @@ namespace Snap
             BOOL bDllExit = FALSE;
             struct IslandEnvironment* pIslandEnvironment = NULL;
 
-            enum struct IslandState : int
-            {
-                None = 0,
-                Error = 1,
-                Started = 2,
-                Stopped = 3,
-            };
+            enum struct IslandState;
 
-            struct IslandEnvironment {
-                LPVOID Address;
-                INT32 Value;
-                enum IslandState State;
-                DWORD LastError;
-                INT32 Reserved;
-            };
+            struct IslandEnvironment;
 
             template <typename THandle, typename TFree>
             class SafeHandle
@@ -47,32 +34,64 @@ namespace Snap
                 THandle m_handle;
                 TFree m_free;
             public:
-                SafeHandle(THandle handle, TFree free) : m_handle(handle), m_free(free)
-                {
-                }
-                ~SafeHandle()
-                {
-                    if (m_handle)
-                    {
-                        m_free(m_handle);
-                    }
-                }
-                THandle Get() const
-                {
-                    return m_handle;
-                }
-                operator THandle() const
-                {
-                    return m_handle;
-                }
-                operator bool() const
-                {
-                    return m_handle != NULL && m_handle != INVALID_HANDLE_VALUE;
-                }
+                SafeHandle(THandle, TFree);
+                ~SafeHandle();
+                THandle Get() const;
+                operator THandle() const;
+                operator bool() const;
             };
 
             using SafeFileHandle = SafeHandle<HANDLE, decltype(&CloseHandle)>;
             using SafeMappedView = SafeHandle<LPVOID, decltype(&UnmapViewOfFile)>;
         }
     }
+}
+
+enum struct Snap::Hutao::UnlockerIsland::IslandState : int
+{
+    None = 0,
+    Error = 1,
+    Started = 2,
+    Stopped = 3,
+};
+
+struct Snap::Hutao::UnlockerIsland::IslandEnvironment
+{
+    LPVOID Address;
+    INT32 Value;
+    enum IslandState State;
+    DWORD LastError;
+    INT32 Reserved;
+};
+
+template <typename THandle, typename TFree>
+Snap::Hutao::UnlockerIsland::SafeHandle<THandle, TFree>::SafeHandle(THandle handle, TFree free) : m_handle(handle), m_free(free)
+{
+}
+
+template <typename THandle, typename TFree>
+Snap::Hutao::UnlockerIsland::SafeHandle<THandle, TFree>::~SafeHandle()
+{
+    if (m_handle)
+    {
+        m_free(m_handle);
+    }
+}
+
+template <typename THandle, typename TFree>
+THandle Snap::Hutao::UnlockerIsland::SafeHandle<THandle, TFree>::Get() const
+{
+    return m_handle;
+}
+
+template <typename THandle, typename TFree>
+Snap::Hutao::UnlockerIsland::SafeHandle<THandle, TFree>::operator THandle() const
+{
+    return m_handle;
+}
+
+template <typename THandle, typename TFree>
+Snap::Hutao::UnlockerIsland::SafeHandle<THandle, TFree>::operator bool() const
+{
+    return m_handle != NULL && m_handle != INVALID_HANDLE_VALUE;
 }

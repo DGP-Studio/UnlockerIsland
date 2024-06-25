5e1432c182953598f878203ae8e80efb0fdce116
Lightczx <1686188646@qq.com>
2024-06-25 10:27:44 +0800

    [skip ci] remove empty line

--- src/UnlockerIsland/Snap.Hutao.UnlockerIsland/dllmain.cpp
+++ src/UnlockerIsland/Snap.Hutao.UnlockerIsland/dllmain.cpp
@@ -64,7 +64,6 @@ BOOL APIENTRY DllMain(HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpReser
         return TRUE;
     }
 
-
     switch (ul_reason_for_call)
     {
         case DLL_PROCESS_ATTACH:
